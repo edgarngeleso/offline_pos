bb09e9ec48c2fdd7759527521de873c6109ad0b8
Edgar Ngeleso <edgarngereso@gmail.com>
2025-02-24 08:41:37 +0300

    Maps integration

--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -4,5 +4,5 @@ export const CONTACT_EMAIL = "support@offline_pos.com";
 
 export const APP_LINK = "https://play.google.com?id=com.offline_pos";
 
-export const GOOGLE_MAPS_API_KEY = "AIzaSyBE5ve0AcSx8RBiMd7O-tISK8o6GjjcUaM";
+export const GOOGLE_MAPS_API_KEY = "AIzaSyBE5ve0AcSx8RBiMd7O-tISK8o6Gjjc";
 
